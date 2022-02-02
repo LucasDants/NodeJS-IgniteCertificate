f33a04df64c7906a78710734707353d9b12ef9e0
Lucas Dantas <lucasdantasmoura01@gmail.com>
2022-02-02 16:11:36 -0300

    gerando certificado em pdf

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,10 +1,30 @@
 import { APIGatewayProxyHandler } from "aws-lambda"
 import { document } from "../utils/dynamodbClient"
-
+import { compile as handlebarsCompile } from 'handlebars'
+import { join } from 'path'
+import { readFileSync } from 'fs'
+import dayjs from 'dayjs'
+import chromiun from 'chrome-aws-lambda'
 interface ICreateCertificate {
     id: string
     name: string
     grade: string 
+}
+
+interface ITemplate {
+    id: string
+    name: string
+    grade: string
+    medal: string
+    date: string
+}
+
+const compileTemplate = async (data: ITemplate) => {
+    const filePath = join(process.cwd(), 'src', 'templates', 'certificate.hbs')
+
+    const html = readFileSync(filePath, 'utf-8')
+
+    return handlebarsCompile(html)(data)
 }
 
 export const handler: APIGatewayProxyHandler = async (event) => {
@@ -28,7 +48,42 @@ export const handler: APIGatewayProxyHandler = async (event) => {
             ":id": id
         }
     }).promise()
+
+    const medalPath = join(process.cwd(), 'src', 'templates', 'selo.png')
+    const medal = readFileSync(medalPath, 'base64')
+
+    const data: ITemplate =  {
+        name,
+        id,
+        grade,
+        date: dayjs().format('DD/MM/YYYY'),
+        medal
+    }
+
+    const content = await compileTemplate(data)
     
+    const browser = await chromiun.puppeteer.launch({
+        args: chromiun.args,
+        defaultViewport: chromiun.defaultViewport,
+        executablePath: await chromiun.executablePath,
+    })
+
+    const page = await browser.newPage()
+
+    await page.setContent(content)
+
+    const pdf =  await page.pdf({
+        format: "a4",
+        landscape: true,
+        printBackground: true,
+        preferCSSPageSize: true,
+        path: process.env.IS_OFFLINE ? './certificate.pdf' : null
+    })
+    
+    await browser.close()
+
+
+
     return {
         statusCode: 201,
         body: JSON.stringify({
